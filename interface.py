92d30d7a2b4cc46e8a849449577df91b29d4059a
dev-ezzy <bettezra07@gmail.com>
2025-03-08 01:14:21 +0000

    streamlit application

---
+++ interface.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("TRAFFIC ADND INCIDENTS DASHBOARD")
+st.header("Plan your ride safely with us by knowing how the weather and the traffic conditions in your area are.")
+st.write("Vamos!!!!!")
+
+if st.button("Click Me"):
+    st.success("Fetching your daily feed mate, give me a sec!")
